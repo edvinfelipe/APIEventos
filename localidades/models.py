2573c606a09e516dd822c973edb120fa39761a03
EddyFelipe <eddigzol@gmail.com>
2019-09-16 21:50:00 -0600

    cracion de modelos

--- localidades/models.py
+++ localidades/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class Departamento ( models.Model ):
-    nombre = models.CharField(max_length=60)
+class Localidad( models.Model ):
+    tipo = models.CharField(max_length=45)
+    costo = models.DecimalField(max_digits=8, decimal_places= 4)
+    cantidadAsientos = models.IntegerField()
+    canitidadAsientosDisponible = models.IntegerField()
+    cantidadAsientosOcupados = models.IntegerField()
+    
