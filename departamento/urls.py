efa6c12544986f4574093cf155b3506232d135c6
Felipe <eddigzol@gmail.com>
2019-09-20 16:46:55 -0600

    ingreso de datos a la bd

--- departamento/urls.py
+++ departamento/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from departamento import views
 
 urlpatterns = [
+    path('', views.Bienvenida.as_view()),
     path('api/departamento/', views.DepartamentAPIView.as_view())
 ]
