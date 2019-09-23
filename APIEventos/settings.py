4cde6b2eeba1e4e7a76d554d125b419c6584c01b
Felipe <eddigzol@gmail.com>
2019-09-23 14:08:48 -0600

    borrado de componentes innecesario

--- APIEventos/settings.py
+++ APIEventos/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'APIEventos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ventavoletos',
-        'USER': 'root',
-        'PASSWORD': '4321',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
