328ad1066aa2840ddc305c1ef97268d26eb9754d
Andrés <nsanchezandresca@gmail.com>
2019-09-23 19:46:32 -0600

    Cambio a modelo localidades

--- localidades/models.py
+++ localidades/models.py
@@ -5,7 +5,7 @@ from eventos.models import Evento
 class Localidad( models.Model):
     costo = models.DecimalField(max_digits=15, decimal_places= 4)
     cantidadAsientos = models.IntegerField()
-    canitidadAsientosDisponible = models.IntegerField(blank=True)
+    cantidadAsientosDisponible = models.IntegerField(blank=True)
     cantidadAsientosOcupados = models.IntegerField(blank=True)
     idEventos = models.ForeignKey(Evento, null=True, on_delete=models.CASCADE)
     idTipoLocalidad = models.ForeignKey(TipoLocalidad, null=True, on_delete=models.CASCADE)
