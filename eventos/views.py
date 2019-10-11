93ee72a680dedf7cf95ac03ff5c08b950e06f5ed
Felipe <eddigzol@gmail.com>
2019-10-11 16:01:45 -0600

    Filtrado de evento por fecha

--- eventos/views.py
+++ eventos/views.py
@@ -1,10 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 import random
+import datetime
 from .models import Evento
-from .serializers import EventoSerializers, EventoSerializerModificacion
+from .serializers import EventoSerializers, EventoSerializerModificacion, EventoDepartamentSerializers
 from imagenes import views as views_imagen
 from comentarios import views as views_comentario
+
 
 def convertir_datos_json(data):
     json = {}
@@ -41,7 +43,9 @@ class EventoAPIView(APIView):
         serializer = EventoSerializers(data=jsonevento)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            event1 = Evento.objects.get(codigo=serializer.data.get('codigo'))
+            serializer1 = EventoDepartamentSerializers(instance=event1)
+            return Response(serializer1.data)
         
         return Response(serializer.errors)
     
@@ -54,7 +58,7 @@ class EventoAPIView(APIView):
             except Evento.DoesNotExist:
                 return Response({})
             
-            serializer = EventoSerializers(eventos, many = True) 
+            serializer = EventoDepartamentSerializers(eventos, many = True) 
             return Response(serializer.data)
         
         else:
@@ -63,7 +67,7 @@ class EventoAPIView(APIView):
             except Evento.DoesNotExist:
                 return Response({'Error':'No existe el evento con el codigo enviado'})
             
-            serializer = EventoSerializers(evento, many=False)
+            serializer = EventoDepartamentSerializers(evento, many=False)
             return Response(serializer.data)
     
     def put(self,request):
@@ -84,7 +88,7 @@ class EventoAPIView(APIView):
             
             return Response({'mensaje':'Evento modificado con éxito'})
         return Response({'Error':'Fallo en la modificación'})
-
+        
     def delete(self, request):
         codigo_evento = request.GET.get('codigo')
 
@@ -99,3 +103,17 @@ class EventoAPIView(APIView):
             return Response({'mensaje':'Evento eliminado con éxito'})
         except Evento.DoesNotExist:
             return Response({'Error':'No existe el evento'})
+
+class EventoFilterFecha(APIView):
+
+    def get(self, request):
+        
+        date = datetime.datetime.now().date()
+        
+        try:
+            eventos = Evento.objects.filter(eliminado=False,fecha__gte=date)
+        except Evento.DoesNotExist:
+            return Response({'Error':'Hubo error en la obtención de los eventos'})
+            
+        serializer = EventoDepartamentSerializers(eventos, many = True) 
+        return Response(serializer.data)        
