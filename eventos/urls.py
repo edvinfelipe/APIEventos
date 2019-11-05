bb99838e4d6a576cc3f7956ecbde440253074593
Felipe <eddigzol@gmail.com>
2019-11-05 11:21:09 -0600

    Agregacion de paginacion en eventos

--- eventos/urls.py
+++ eventos/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from  .views import EventoAPIView, EventoFilterFecha
+from  .views import EventoAPIView, EventoFilterFecha, List_evento
 
 urlpatterns = [
     path('api/eventos/', EventoAPIView.as_view()),
-    path('api/eventos/fecha/', EventoFilterFecha.as_view())
+    path('api/eventos/fecha/', EventoFilterFecha.as_view()),
+    path('api/eventos/paginacion/', List_evento.as_view())
 ]
