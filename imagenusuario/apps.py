41186afb1bb3ec7ef09b26fc51895760562e923c
Cristian <cristianmmr09@gmail.com>
2019-11-05 17:30:06 -0600

    Eliminacion de imagenUsuario y modificacion de app Usuario

--- imagenusuario/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ImagenusuarioConfig(AppConfig):
-    name = 'imagenusuario'
--- usuarios/models.py
+++ usuarios/models.py
@@ -5,3 +5,4 @@ from django.db import models
 class Usuario(models.Model):
     nombre = models.CharField(max_length=45)
     correo = models.CharField(max_length=45)
+    rutaImagen = models.CharField(max_length=90)
